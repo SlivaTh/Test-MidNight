aa54a275a0afdd8a38558625688408fc73464475
SlivaTh <80697195+SlivaTh@users.noreply.github.com>
2021-03-15 16:38:26 +0200

    Add empty project

---
+++ Test To MidNight/Assets/Scripts/Color/color.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class color : MonoBehaviour
+{
+    [SerializeField]
+    private Color cl = new Color(0f, 0f, 0f, 0f);
+
+    private void Update()
+    {
+        gameObject.GetComponent<MeshRenderer>().sharedMaterial.color = cl;
+    }
+}
